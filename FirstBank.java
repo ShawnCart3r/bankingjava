3f45e2ed1790eefb49eb7516a1bdd83c29cd4dfb
Shawn Carter <53487411+ShawnCart3r@users.noreply.github.com>
2021-05-02 22:25:34 -0400

    Add files via upload

---
+++ FirstBank.java
@@ -0,0 +1,9 @@
+public class FirstBank {
+    public static void main(String[] args){
+        Account shawn = new Account("Shawn Carter", "A0001");
+        shawn.showMenu();
+
+        Account dusty = new Account("Dusty", "B00034");
+        dusty.showMenu();
+    }
+}
